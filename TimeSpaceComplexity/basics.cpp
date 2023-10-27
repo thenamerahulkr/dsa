81bcbbceb513a1daff1e41a4785c034b3a0fdec6
thenamerahulkr <thenamerahulkr@gmail.com>
2023-10-27 15:13:07 +0530

    TimeSpaceComplexity

---
+++ TimeSpaceComplexity/basics.cpp
@@ -0,0 +1,24 @@
+/*Generally, there is always more than one way to solve a problem in computer science with different algorithms. Therefore, it is highly required to use a method to compare the solutions in order to judge which one is more optimal. The method must be:
+
+Independent of the machine and its configuration, on which the algorithm is running on.
+Shows a direct correlation with the number of inputs.
+Can distinguish two algorithms clearly without ambiguity.*/
+
+/*Time Complexity: The time complexity of an algorithm quantifies the amount of time taken by an algorithm to run as a function of the length of the input. Note that the time to run is a function of the length of the input and not the actual execution time of the machine on which the algorithm is running on.*/
+
+
+
+// SPACE COMPLEXITY
+
+
+/*In computer science, space complexity refers to the amount of memory space required by an algorithm or a program to solve a problem. It is a measure of how much memory an algorithm needs with respect to the input size. Space complexity is an important consideration, especially in scenarios where memory is limited, such as in embedded systems or when dealing with large datasets.
+
+Space complexity can be classified into two types:
+
+1. Auxiliary space complexity: This refers to the extra space used by an algorithm, not including the space taken by the input. It includes space required by variables, data structures, and other internal data.
+
+2. Total space complexity: This refers to the total space used by an algorithm, including both the input space and the auxiliary space.
+
+Space complexity is typically expressed in terms of the number of memory cells or bits required, and it can be described using big O notation in the same way as time complexity. For example, an algorithm with a space complexity of O(1) implies that it requires a constant amount of memory, regardless of the input size, while an algorithm with a space complexity of O(n) means that the memory usage grows linearly with the size of the input.
+
+Analyzing space complexity is crucial for understanding how an algorithm will perform in terms of memory usage and whether it will be feasible for the available hardware resources. It helps in making informed decisions when designing algorithms for specific applications and platforms.*/
